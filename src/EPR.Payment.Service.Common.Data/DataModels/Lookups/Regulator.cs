af9def7e610da9725a08cc7e8edb3b2969727893
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2024-07-25 17:07:18 +0100

    Removed deprecated tables from database and added new tables after approved ADR

---
+++ src/EPR.Payment.Service.Common.Data/DataModels/Lookups/Regulator.cs
@@ -0,0 +1,27 @@
+﻿using EPR.Payment.Service.Common.Data.DataModels.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Payment.Service.Common.Data.DataModels.Lookups
+{
+    [Table("Regulator", Schema = "Lookup")]
+    [ExcludeFromCodeCoverage]
+    public class Regulator : BaseEntity
+    {
+        [MaxLength(50)]
+        public string Type { get; set; } = null!;
+
+        [MaxLength(255)]
+        public string Description { get; set; } = null!;
+
+        #region Navigation properties
+        public virtual ICollection<RegistrationFees> RegistrationFees { get; set; } = null!;
+        #endregion
+    }
+}
---
+++ src/EPR.Payment.Service.Common.Data/DataModels/Lookups/SubGroup.cs
@@ -0,0 +1,22 @@
+﻿using EPR.Payment.Service.Common.Data.DataModels.BaseClasses;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Payment.Service.Common.Data.DataModels.Lookups
+{
+    [Table("SubGroup", Schema = "Lookup")]
+    [ExcludeFromCodeCoverage]
+    public class SubGroup : BaseEntity
+    {
+        [MaxLength(50)]
+        public string Type { get; set; } = null!;
+
+        [MaxLength(255)]
+        public string Description { get; set; } = null!;
+
+        #region Navigation properties
+        public virtual ICollection<RegistrationFees> RegistrationFees { get; set; } = null!;
+        #endregion
+    }
+}
