921e947eec2a32164216142af93511a7d098299b
manishagarwalEviden <167305073+manishagarwalEviden@users.noreply.github.com>
2024-08-06 15:04:53 +0100

    Feature/417653 updated migration script (#12)
    * epr-payment-service pipeline
    
    * epr-payment-service pipeline
    
    * updated gitignore file
    
    * epr-payment-service pipeline
    
    * epr-payment-service pipeline
    
    * migration files
    
    * migration files
    
    * migration files
    
    * Update ci_pipeline.yaml
    
    * Update ci_pipeline.yaml
    
    * epr-payment-service  pipeline
    
    * migration files
    
    * initialise-SQL-db  pipeline
    
    * inisitalise SQL migration
    
    * inisitalise SQL migration for dev 1
    
    * inisitalise SQL migration for dev 1
    
    * inisitalise SQL migration for dev 11
    
    * inisitalise SQL migration for dev 11
    
    * inisitalise SQL migration for dev 11
    
    * Dev 4 deployment added to the pipeline
    
    * 417653
    
    ---------
    
    Co-authored-by: Himat Varsani <himat.varsani@eviden.com>
    Co-authored-by: Nagendra Kommuru (ext) <nagendra.kommuru.external@eviden.com>
    Co-authored-by: himatvarsani-atos <156190828+himatvarsani-atos@users.noreply.github.com>

--- src/EPR.Payment.Service.Common.Data/Scripts/migrations.sql
+++ src/EPR.Payment.Service.Common.Data/Scripts/migrations.sql
@@ -286,3 +286,251 @@ GO
 COMMIT;
 GO
 
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    DROP TABLE [Lookup].[AdditionalRegistrationFees];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    DROP TABLE [Lookup].[ComplianceSchemeRegistrationFees];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    DROP TABLE [Lookup].[ProducerRegistrationFees];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    DROP TABLE [Lookup].[SubsidiariesRegistrationFees];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    CREATE TABLE [Lookup].[Group] (
+        [Id] int NOT NULL IDENTITY,
+        [Type] nvarchar(50) NOT NULL,
+        [Description] nvarchar(255) NOT NULL,
+        CONSTRAINT [PK_Group] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    CREATE TABLE [Lookup].[Regulator] (
+        [Id] int NOT NULL IDENTITY,
+        [Type] nvarchar(50) NOT NULL,
+        [Description] nvarchar(255) NOT NULL,
+        CONSTRAINT [PK_Regulator] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    CREATE TABLE [Lookup].[SubGroup] (
+        [Id] int NOT NULL IDENTITY,
+        [Type] nvarchar(50) NOT NULL,
+        [Description] nvarchar(255) NOT NULL,
+        CONSTRAINT [PK_SubGroup] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    CREATE TABLE [Lookup].[RegistrationFees] (
+        [Id] int NOT NULL IDENTITY,
+        [GroupId] int NOT NULL,
+        [SubGroupId] int NOT NULL,
+        [RegulatorId] int NOT NULL,
+        [Amount] decimal(19,4) NOT NULL,
+        [EffectiveFrom] datetime2 NOT NULL,
+        [EffectiveTo] datetime2 NOT NULL,
+        CONSTRAINT [PK_RegistrationFees] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_RegistrationFees_Group_GroupId] FOREIGN KEY ([GroupId]) REFERENCES [Lookup].[Group] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_RegistrationFees_Regulator_RegulatorId] FOREIGN KEY ([RegulatorId]) REFERENCES [Lookup].[Regulator] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_RegistrationFees_SubGroup_SubGroupId] FOREIGN KEY ([SubGroupId]) REFERENCES [Lookup].[SubGroup] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Description', N'Type') AND [object_id] = OBJECT_ID(N'[Lookup].[Group]'))
+        SET IDENTITY_INSERT [Lookup].[Group] ON;
+    EXEC(N'INSERT INTO [Lookup].[Group] ([Id], [Description], [Type])
+    VALUES (1, N''Producer Type'', N''ProducerType''),
+    (2, N''Compliance Scheme'', N''ComplianceScheme''),
+    (3, N''Producer Subsidiaries'', N''ProducerSubsidiaries''),
+    (4, N''Compliance Scheme Subsidiaries'', N''ComplianceSchemeSubsidiaries'')');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Description', N'Type') AND [object_id] = OBJECT_ID(N'[Lookup].[Group]'))
+        SET IDENTITY_INSERT [Lookup].[Group] OFF;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Description', N'Type') AND [object_id] = OBJECT_ID(N'[Lookup].[Regulator]'))
+        SET IDENTITY_INSERT [Lookup].[Regulator] ON;
+    EXEC(N'INSERT INTO [Lookup].[Regulator] ([Id], [Description], [Type])
+    VALUES (1, N''England'', N''GB-ENG''),
+    (2, N''Scotland'', N''GB-SCT''),
+    (3, N''Wales'', N''GB-WLS''),
+    (4, N''Northern Ireland'', N''GB-NIR'')');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Description', N'Type') AND [object_id] = OBJECT_ID(N'[Lookup].[Regulator]'))
+        SET IDENTITY_INSERT [Lookup].[Regulator] OFF;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Description', N'Type') AND [object_id] = OBJECT_ID(N'[Lookup].[SubGroup]'))
+        SET IDENTITY_INSERT [Lookup].[SubGroup] ON;
+    EXEC(N'INSERT INTO [Lookup].[SubGroup] ([Id], [Description], [Type])
+    VALUES (1, N''Large producer'', N''Large''),
+    (2, N''Small producer'', N''Small''),
+    (3, N''Registration'', N''Registration''),
+    (4, N''Online Market'', N''Online''),
+    (5, N''Up to 20'', N''UpTo20''),
+    (6, N''More than 20'', N''MoreThan20'')');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Description', N'Type') AND [object_id] = OBJECT_ID(N'[Lookup].[SubGroup]'))
+        SET IDENTITY_INSERT [Lookup].[SubGroup] OFF;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Amount', N'EffectiveFrom', N'EffectiveTo', N'GroupId', N'RegulatorId', N'SubGroupId') AND [object_id] = OBJECT_ID(N'[Lookup].[RegistrationFees]'))
+        SET IDENTITY_INSERT [Lookup].[RegistrationFees] ON;
+    EXEC(N'INSERT INTO [Lookup].[RegistrationFees] ([Id], [Amount], [EffectiveFrom], [EffectiveTo], [GroupId], [RegulatorId], [SubGroupId])
+    VALUES (1, 2620.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 1, 1, 1),
+    (2, 2620.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 1, 2, 1),
+    (3, 2620.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 1, 3, 1),
+    (4, 2620.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 1, 4, 1),
+    (5, 1216.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 1, 1, 2),
+    (6, 1216.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 1, 2, 2),
+    (7, 1216.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 1, 3, 2),
+    (8, 1216.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 1, 4, 2),
+    (9, 1658.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 1, 1),
+    (10, 1658.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 2, 1),
+    (11, 1658.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 3, 1),
+    (12, 1658.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 4, 1),
+    (13, 631.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 1, 2),
+    (14, 631.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 2, 2),
+    (15, 631.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 3, 2),
+    (16, 631.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 4, 2),
+    (17, 13804.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 1, 3),
+    (18, 13804.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 2, 3),
+    (19, 13804.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 3, 3),
+    (20, 13804.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 4, 3),
+    (21, 2579.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 1, 4),
+    (22, 2579.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 2, 4),
+    (23, 2579.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 3, 4),
+    (24, 2579.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 2, 4, 4),
+    (25, 558.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 3, 1, 5),
+    (26, 558.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 3, 2, 5),
+    (27, 558.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 3, 3, 5),
+    (28, 558.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 3, 4, 5),
+    (29, 140.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 3, 1, 6),
+    (30, 140.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 3, 2, 6),
+    (31, 140.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 3, 3, 6),
+    (32, 140.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 3, 4, 6),
+    (33, 558.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 4, 1, 5),
+    (34, 558.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 4, 2, 5),
+    (35, 558.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 4, 3, 5),
+    (36, 558.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 4, 4, 5),
+    (37, 140.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 4, 1, 6),
+    (38, 140.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 4, 2, 6),
+    (39, 140.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 4, 3, 6),
+    (40, 140.0, ''2025-01-01T00:00:00.0000000'', ''2025-12-31T00:00:00.0000000'', 4, 4, 6)');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Amount', N'EffectiveFrom', N'EffectiveTo', N'GroupId', N'RegulatorId', N'SubGroupId') AND [object_id] = OBJECT_ID(N'[Lookup].[RegistrationFees]'))
+        SET IDENTITY_INSERT [Lookup].[RegistrationFees] OFF;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    CREATE INDEX [IX_RegistrationFees_GroupId] ON [Lookup].[RegistrationFees] ([GroupId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    CREATE INDEX [IX_RegistrationFees_RegulatorId] ON [Lookup].[RegistrationFees] ([RegulatorId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    CREATE INDEX [IX_RegistrationFees_SubGroupId] ON [Lookup].[RegistrationFees] ([SubGroupId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240725154904_RegistrationFeesTablesUpdate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20240725154904_RegistrationFeesTablesUpdate', N'8.0.4');
+END;
+GO
+
+COMMIT;
+GO
+
