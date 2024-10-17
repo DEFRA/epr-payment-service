8bc0913a36578701a866a9acd53705f0f6b824d6
Paul Shimmen <167417965+paulshimmen@users.noreply.github.com>
2024-10-17 12:12:56 +0100

    Feature/tasks/459887 restructure folders for service (#57)
    * Changing folder structure for resubmission
    
    * Changing folder name for constants
    
    * Additional folder structure changes
    
    * Folder structure changes for tests

--- src/EPR.Payment.Service/Strategies/Interfaces/ResubmissionFees/Producer/IResubmissionAmountStrategy.cs
+++ src/EPR.Payment.Service/Strategies/Interfaces/ResubmissionFees/Producer/IResubmissionAmountStrategy.cs
@@ -1,6 +1,7 @@
 ﻿using EPR.Payment.Service.Common.Dtos.Request.RegistrationFees.Producer;
+using EPR.Payment.Service.Strategies.Interfaces.RegistrationFees;
 
-namespace EPR.Payment.Service.Strategies.Interfaces.RegistrationFees.Producer
+namespace EPR.Payment.Service.Strategies.Interfaces.ResubmissionFees.Producer
 {
     public interface IResubmissionAmountStrategy<TRequestDto, TResponseDto> : IFeeCalculationStrategy<TRequestDto, TResponseDto>
     {
