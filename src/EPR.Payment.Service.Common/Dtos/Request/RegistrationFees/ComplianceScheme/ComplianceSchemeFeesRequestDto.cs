d9a7d04c57b2db462ec6250c76b086dc1b894c20
David Jayakumar <david.2.jayakumar.external@eviden.com>
2024-10-18 11:26:50 +0100

    Add logic for late fee

--- src/EPR.Payment.Service.Common/Dtos/Request/RegistrationFees/ComplianceScheme/ComplianceSchemeFeesRequestDto.cs
+++ src/EPR.Payment.Service.Common/Dtos/Request/RegistrationFees/ComplianceScheme/ComplianceSchemeFeesRequestDto.cs
@@ -12,6 +12,7 @@
         public required string MemberId { get; set; }
         public required string MemberType { get; set; } //"Large" or "Small"
         public bool IsOnlineMarketplace { get; set; }
+        public bool IsLateFeeApplicable { get; set; }
         public int NumberOfSubsidiaries { get; set; }
         public int NoOfSubsidiariesOnlineMarketplace { get; set; }
     }
