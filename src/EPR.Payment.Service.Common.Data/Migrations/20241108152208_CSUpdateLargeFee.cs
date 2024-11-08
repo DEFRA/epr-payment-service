e05b07e2e52e504d424af4f7bf43d5ba40a0b774
Zozan KIRKUTAY <167984276+zozanKirkutay@users.noreply.github.com>
2024-11-08 16:20:54 +0000

    feature/tasks/471319 update large fee CS (#98)

---
+++ src/EPR.Payment.Service.Common.Data/Migrations/20241108152208_CSUpdateLargeFee.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Payment.Service.Common.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CSUpdateLargeFee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Lookup",
+                table: "RegistrationFees",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Amount",
+                value: 168500m);
+
+            migrationBuilder.UpdateData(
+                schema: "Lookup",
+                table: "RegistrationFees",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Amount",
+                value: 168500m);
+
+            migrationBuilder.UpdateData(
+                schema: "Lookup",
+                table: "RegistrationFees",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Amount",
+                value: 168500m);
+
+            migrationBuilder.UpdateData(
+                schema: "Lookup",
+                table: "RegistrationFees",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Amount",
+                value: 168500m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Lookup",
+                table: "RegistrationFees",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Amount",
+                value: 165800m);
+
+            migrationBuilder.UpdateData(
+                schema: "Lookup",
+                table: "RegistrationFees",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Amount",
+                value: 165800m);
+
+            migrationBuilder.UpdateData(
+                schema: "Lookup",
+                table: "RegistrationFees",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Amount",
+                value: 165800m);
+
+            migrationBuilder.UpdateData(
+                schema: "Lookup",
+                table: "RegistrationFees",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Amount",
+                value: 165800m);
+        }
+    }
+}
