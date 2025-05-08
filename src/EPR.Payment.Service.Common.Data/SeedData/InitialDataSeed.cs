117630a600781e083482426d86f49d09d26a0fd5
Rekha Devi <rekha.devi.external@eviden.com>
2025-05-08 09:23:10 +0100

    Added seed data for group and subgroup tables

--- src/EPR.Payment.Service.Common.Data/SeedData/InitialDataSeed.cs
+++ src/EPR.Payment.Service.Common.Data/SeedData/InitialDataSeed.cs
@@ -22,25 +22,9 @@ namespace EPR.Payment.Service.Common.Data.SeedData
                 new PaymentStatus { Id = Enums.Status.UserCancelled, Status = "UserCancelled" }
                 );
 
-            modelBuilder.Entity<Group>().HasData(
-               new Group { Id = 1, Type = "ProducerType", Description = "Producer Type" },
-               new Group { Id = 2, Type = "ComplianceScheme", Description = "Compliance Scheme" },
-               new Group { Id = 3, Type = "ProducerSubsidiaries", Description = "Producer Subsidiaries" },
-               new Group { Id = 4, Type = "ComplianceSchemeSubsidiaries", Description = "Compliance Scheme Subsidiaries" },
-               new Group { Id = 5, Type = "ProducerResubmission", Description = "Producer re-submitting a report" },
-               new Group { Id = 6, Type = "ComplianceSchemeResubmission", Description = "Compliance Scheme re-submitting a report" }
-               );
+            modelBuilder.Entity<Group>().HasData(SeedGroupData.GetSeedData());
 
-            modelBuilder.Entity<SubGroup>().HasData(
-               new SubGroup { Id = 1, Type = "Large", Description = "Large producer" },
-               new SubGroup { Id = 2, Type = "Small", Description = "Small producer" },
-               new SubGroup { Id = 3, Type = "Registration", Description = "Registration" },
-               new SubGroup { Id = 4, Type = "Online", Description = "Online Market" },
-               new SubGroup { Id = 5, Type = "UpTo20", Description = "Up to 20" },
-               new SubGroup { Id = 6, Type = "MoreThan20", Description = "More than 20" },
-               new SubGroup { Id = 7, Type = "ReSubmitting", Description = "Re-submitting a report" },
-               new SubGroup { Id = 8, Type = "LateFee", Description = "Late Fee" }
-               );
+            modelBuilder.Entity<SubGroup>().HasData(SeedSubGroupData.GetSeedData());
 
             modelBuilder.Entity<Regulator>().HasData(
                new Regulator { Id = 1, Type = "GB-ENG", Description = "England" },
---
+++ src/EPR.Payment.Service.Common.Data/SeedData/SeedGroupData.cs
@@ -0,0 +1,22 @@
+﻿using EPR.Payment.Service.Common.Data.DataModels.Lookups;
+
+namespace EPR.Payment.Service.Common.Data.SeedData
+{
+    public class SeedGroupData
+    {
+        public static List<Group> GetSeedData()
+        {
+            return new List<Group>
+            {
+               new Group { Id = 1, Type = "ProducerType", Description = "Producer Type" },
+               new Group { Id = 2, Type = "ComplianceScheme", Description = "Compliance Scheme" },
+               new Group { Id = 3, Type = "ProducerSubsidiaries", Description = "Producer Subsidiaries" },
+               new Group { Id = 4, Type = "ComplianceSchemeSubsidiaries", Description = "Compliance Scheme Subsidiaries" },
+               new Group { Id = 5, Type = "ProducerResubmission", Description = "Producer re-submitting a report" },
+               new Group { Id = 6, Type = "ComplianceSchemeResubmission", Description = "Compliance Scheme re-submitting a report" },
+               new Group { Id = 7, Type = "Exporters", Description = "Exporters" },
+               new Group { Id = 8, Type = "Reprocessors", Description = "Reprocessors" },
+            };
+        }
+    }
+}
