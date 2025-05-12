d96b955583285220bca0bd8da466e88cfce4a0b8
Jignesh Jethwani (ext) <jignesh.jethwani.external@eviden.com>
2025-05-12 18:02:01 +0100

    Initial Commit to see accreditation fees exporter data

---
+++ src/EPR.Payment.Service.Common.Data/Enums/Regulator.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace EPR.Payment.Service.Common.Data.Enums
+{
+    public enum Regulator
+    {
+        [Description("England")]
+        England = 1,
+
+        [Description("Scotland")]
+        Scotland = 2,
+
+        [Description("Wales")]
+        Wales = 3,
+
+        [Description("Northern Ireland")]
+        NorthernIreland = 4,
+    }
+}
