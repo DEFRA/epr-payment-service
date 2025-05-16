4e1b33f27d47174eb51d79a2729528b42002db6a
mahexed <mahmood.mahmood.external@eviden.com>
2025-05-16 11:01:32 +0100

    Feature/550541 seed accreditation table reprocessors (#137)
    * seed accreditation table reprocessors
    
    * Create migration using idempotent
    
    ---------
    
    Co-authored-by: mahmood mahmood (Eviden) <mahmood.mahmood@Defra.onmicrosoft.com>

---
+++ src/EPR.Payment.Service.Common.Data/Migrations/SQLScripts/20250513003609_SeedAccreditationFeesTablesReprocessors_Rollback.sql
@@ -0,0 +1,586 @@
+﻿BEGIN TRANSACTION;
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 97;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 98;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 99;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 100;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 101;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 102;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 103;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 104;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 105;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 106;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 107;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 108;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 109;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 110;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 111;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 112;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 113;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 114;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 115;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 116;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 117;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 118;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 119;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 120;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 121;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 122;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 123;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 124;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 125;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 126;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 127;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 128;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 129;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 130;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 131;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 132;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 133;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 134;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 135;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 136;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 137;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 138;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 139;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 140;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 141;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 142;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 143;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 144;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 145;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 146;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 147;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 148;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 149;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 150;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 151;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 152;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 153;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 154;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 155;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 156;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 157;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 158;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 159;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 160;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 161;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 162;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 163;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 164;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 165;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 166;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 167;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 168;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 169;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 170;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 171;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 172;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 173;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 174;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 175;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 176;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 177;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 178;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 179;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 180;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 181;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 182;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 183;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 184;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 185;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 186;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 187;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 188;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 189;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 190;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 191;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [Lookup].[AccreditationFees]
+WHERE [Id] = 192;
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [__EFMigrationsHistory]
+WHERE [MigrationId] = N'20250513003609_SeedAccreditationFeesTablesReprocessors';
+GO
+
+COMMIT;
+GO
+
