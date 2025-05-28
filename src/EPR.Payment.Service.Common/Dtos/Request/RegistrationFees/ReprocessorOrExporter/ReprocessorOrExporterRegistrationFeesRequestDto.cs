41cf18cab49d55b5c3872f9c12ccb0a4a0276fd4
Lee Carswell (ext) <lee.carswell.external@eviden.com>
2025-05-28 15:28:45 +0100

    Added JsonConverter attributes

--- src/EPR.Payment.Service.Common/Dtos/Request/RegistrationFees/ReprocessorOrExporter/ReprocessorOrExporterRegistrationFeesRequestDto.cs
+++ src/EPR.Payment.Service.Common/Dtos/Request/RegistrationFees/ReprocessorOrExporter/ReprocessorOrExporterRegistrationFeesRequestDto.cs
@@ -1,15 +1,18 @@
-﻿using EPR.Payment.Service.Common.Enums;
+﻿using System.Text.Json.Serialization;
+using EPR.Payment.Service.Common.Enums;
 
 namespace EPR.Payment.Service.Common.Dtos.Request.RegistrationFees.ReprocessorOrExporter
 {
     public class ReprocessorOrExporterRegistrationFeesRequestDto
     {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public RequestorTypes? RequestorType { get; set; } 
 
         public required string Regulator { get; set; } // "GB-ENG", "GB-SCT", etc.
 
         public required DateTime SubmissionDate { get; set; }
 
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public MaterialTypes? MaterialType { get; set; }
 
         public string? ApplicationReferenceNumber { get; set; } 
--- src/EPR.Payment.Service.Common/Dtos/Response/RegistrationFees/ReprocessorOrExporter/ReprocessorOrExporterRegistrationFeesResponseDto.cs
+++ src/EPR.Payment.Service.Common/Dtos/Response/RegistrationFees/ReprocessorOrExporter/ReprocessorOrExporterRegistrationFeesResponseDto.cs
@@ -1,9 +1,11 @@
-﻿using EPR.Payment.Service.Common.Enums;
+﻿using System.Text.Json.Serialization;
+using EPR.Payment.Service.Common.Enums;
 
 namespace EPR.Payment.Service.Common.Dtos.Response.RegistrationFees.ReprocessorOrExporter
 {
     public class ReprocessorOrExporterRegistrationFeesResponseDto
     {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public MaterialTypes? MaterialType { get; set; }
 
         public required decimal RegistrationFee { get; set; }
