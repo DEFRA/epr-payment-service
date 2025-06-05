6dc9339b7c9f5ff6fb6b44b7fc4181711ce660a9
mahmood mahmood (Eviden) <mahmood.mahmood@Defra.onmicrosoft.com>
2025-06-05 14:04:05 +0100

    Extend online payments response

--- src/EPR.Payment.Service.Common.Data/Profiles/PaymentProfile.cs
+++ src/EPR.Payment.Service.Common.Data/Profiles/PaymentProfile.cs
@@ -18,7 +18,8 @@ namespace EPR.Payment.Service.Common.Data.Profiles
 
             CreateMap<DataModels.Payment, OnlinePaymentResponseDto>().ForMember(dest => dest.GovPayPaymentId, opt => opt.MapFrom(src => src.OnlinePayment.GovPayPaymentId))
                                                                      .ForMember(dest => dest.UpdatedByOrganisationId, opt => opt.MapFrom(src => src.OnlinePayment.UpdatedByOrgId))
-                                                                     .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.ReasonForPayment));
+                                                                     .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.ReasonForPayment))
+                                                                     .ForMember(dest => dest.RequestorType, opt => opt.MapFrom(src => src.OnlinePayment.RequestorType));
 
             CreateMap<OfflinePaymentInsertRequestDto, DataModels.Payment>().ForMember(dest => dest.ReasonForPayment, opt => opt.MapFrom(src => src.Description));
 
