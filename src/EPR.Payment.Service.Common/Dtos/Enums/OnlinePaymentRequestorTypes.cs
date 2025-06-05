975e99b7e20f97f6fd21d1ca42ac536fbc1dc5e7
Lee Carswell (ext) <lee.carswell.external@eviden.com>
2025-06-05 15:29:38 +0100

    updated RequestorTypes

--- src/EPR.Payment.Service.Common/Dtos/Enums/OnlinePaymentRequestorTypes.cs
+++ src/EPR.Payment.Service.Common/Dtos/Enums/OnlinePaymentRequestorTypes.cs
@@ -4,9 +4,9 @@ namespace EPR.Payment.Service.Common.Dtos.Enums
 {
     public enum OnlinePaymentRequestorTypes
     {
-        ProducerType = Group.ProducerType,
-        ComplianceScheme = Group.ComplianceScheme,
-        Exporters = Group.Exporters,
-        Reprocessors = Group.Reprocessors,
+        Producers,
+        ComplianceSchemes,
+        Exporters,
+        Reprocessors,
     }
 }
