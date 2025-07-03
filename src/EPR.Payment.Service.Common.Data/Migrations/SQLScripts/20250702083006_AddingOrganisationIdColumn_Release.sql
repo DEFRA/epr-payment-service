f0e1503bda15fd849212462acfdf407ae4068021
msgeviden <manraj.singh.external@atos.net>
2025-07-03 09:53:18 +0100

    Features/576859 add organisation id column to offlinepayment (#151)
    * Added Orginsation Id column to Offline Payment table and ef migeration as well
    
    * Added Release and Rollback SQL Scripts and Update migrations.sql to align with latest changes

---
+++ src/EPR.Payment.Service.Common.Data/Migrations/SQLScripts/20250702083006_AddingOrganisationIdColumn_Release.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [OfflinePayment] ADD [OrganisationId] uniqueidentifier NULL;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250702083006_AddingOrganisationIdColumn', N'8.0.4');
+GO
+
+COMMIT;
+GO
+
