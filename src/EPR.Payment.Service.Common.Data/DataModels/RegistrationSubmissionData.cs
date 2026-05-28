c20addde7891a50a4d24cf334bae2203735d194a
rmohammed-ee <richard.mohammed@equalexperts.com>
2026-05-28 17:11:52 +0100

    Add RegistrationSubmissionData entity and update DbContext (#186)
    * Add RegistrationSubmissionData entity and update DbContext
    
    * Add CreatedDate and UpdatedDate properties to RegistrationSubmissionData entity and create corresponding migration
    
    * Add RegistrationSubmissionData properties and configuration
    
    * Add migration for RegistrationSubmissionData table and update model snapshot

---
+++ src/EPR.Payment.Service.Common.Data/DataModels/RegistrationSubmissionData.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Payment.Service.Common.Data.DataModels
+{
+    [ExcludeFromCodeCoverage]
+    public class RegistrationSubmissionData
+    {
+        public Guid Id { get; set; }
+
+        public Guid SubmissionId { get; set; }
+
+        public Guid FileId { get; set; }
+
+        public string FileName { get; set; } = null!;
+
+        public Guid? ComplianceSchemeId { get; set; }
+
+        public string SubmissionPeriod { get; set; } = null!;
+
+        public DateTime SubmissionDate { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+
+        public DateTimeOffset? UpdatedDate { get; set; }
+    }
+}
