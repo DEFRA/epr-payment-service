548b2e69785ae9e0cc204a19f567755a75ad97c5
Mark Jones <mark.john.jones@esynergy.co.uk>
2026-07-20 11:40:19 +0100

    SUB-300: Added UpgradeSnapshotToEfCore10 migration to migrations collection

---
+++ src/EPR.Payment.Service.Common.Data/Migrations/20260720102344_UpgradeSnapshotToEfCore10.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Payment.Service.Common.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpgradeSnapshotToEfCore10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
