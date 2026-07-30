00e272d6391ab6b972040120addc048fb5105661
Killian Davies <killian.davies@esynergy.co.uk>
2026-07-30 10:03:58 +0100

    remove redundant feature flag

--- src/EPR.Payment.Service/Controllers/RegistrationFees/ComplianceScheme/ComplianceSchemeFeesController.cs
+++ src/EPR.Payment.Service/Controllers/RegistrationFees/ComplianceScheme/ComplianceSchemeFeesController.cs
@@ -94,7 +94,6 @@ namespace EPR.Payment.Service.Controllers.RegistrationFees.ComplianceScheme
         [ProducesResponseType(typeof(ComplianceSchemeFeesResponseDto), 200)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [FeatureGate("EnableComplianceSchemeFees")]
         public async Task<ActionResult<ComplianceSchemeFeesResponseDto>> GetFeesBySubmissionAsync(Guid submissionId, CancellationToken cancellationToken)
         {
             var result = await _feeBySubmissionService.GetFeesAsync(submissionId, cancellationToken);
