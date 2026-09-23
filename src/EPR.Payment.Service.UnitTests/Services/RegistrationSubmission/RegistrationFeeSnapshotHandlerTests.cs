8e2608054a870a64c0005fcc06949a9e81e9e8be
Killian Davies <killian.davies@esynergy.co.uk>
2026-09-23 11:57:52 +0100

    extra test coverage - sonarqube

--- src/EPR.Payment.Service.UnitTests/Services/RegistrationSubmission/RegistrationFeeSnapshotHandlerTests.cs
+++ src/EPR.Payment.Service.UnitTests/Services/RegistrationSubmission/RegistrationFeeSnapshotHandlerTests.cs
@@ -114,6 +114,12 @@ namespace EPR.Payment.Service.UnitTests.Services.RegistrationSubmission
                         CountOfOMPSubsidiaries = 2,
                         UnitOMPFees = 25m,
                         TotalSubsidiariesOMPFees = 50m,
+                        CountOfClosedLoopRecyclingSubsidiaries = 3,
+                        UnitClosedLoopRecyclingFees = 10m,
+                        TotalSubsidiariesClosedLoopRecyclingFees = 30m,
+                        CountOfLateSubsidiaries = 1,
+                        UnitSubsidiaryLateFee = 40m,
+                        TotalSubsidiariesLateFees = 40m,
                     },
                 });
 
@@ -136,6 +142,8 @@ namespace EPR.Payment.Service.UnitTests.Services.RegistrationSubmission
                 captured.LineItems.Should().Contain(l => l.FeeTypeId == FeeTypeIds.ProducerLateRegistrationFee && l.Amount == 250m);
                 captured.LineItems.Should().Contain(l => l.FeeTypeId == FeeTypeIds.SubsidiaryFee && l.BandNumber == 1 && l.Amount == 300m);
                 captured.LineItems.Should().Contain(l => l.FeeTypeId == FeeTypeIds.SubsidiaryOnlineMarketplaceFee && l.Amount == 50m && l.Quantity == 2);
+                captured.LineItems.Should().Contain(l => l.FeeTypeId == FeeTypeIds.SubsidiaryClosedLoopRecyclingFee && l.Amount == 30m && l.Quantity == 3);
+                captured.LineItems.Should().Contain(l => l.FeeTypeId == FeeTypeIds.SubsidiaryLateFee && l.Amount == 40m && l.Quantity == 1);
             }
         }
 
